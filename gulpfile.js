b5cc5742cfb53d0f616c70d04f0b5eeafffd58d2
Nik-WebDev <kolya395451@gmail.com>
2020-05-27 19:45:03 +0600

    added New-Items page and adaptive header and footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 let gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    browserSync = require("browser-sync"),
-    uglify = require("gulp-uglify"),
-    concat = require("gulp-concat"),
-    rename = require("gulp-rename"),
-    cssmin = require("gulp-cssmin"),
-    autoprefixer = require("gulp-autoprefixer");
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  rename = require("gulp-rename"),
+  cssmin = require("gulp-cssmin"),
+  autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function () {
   return (
@@ -41,6 +41,7 @@ gulp.task("style", function () {
       "node_modules/magnific-popup/dist/magnific-popup.css",
       "node_modules/normalize.css/normalize.css",
       "node_modules/rateyo/src/jquery.rateyo.css",
+      "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -52,8 +53,9 @@ gulp.task("script", function () {
     .src([
       "node_modules/slick-carousel/slick/slick.js",
       "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-      "node_modules/mixitup/dist/mixitup.js",
+      "node_modules/mixitup/dist/mixitup.min.js",
       "node_modules/rateyo/src/jquery.rateyo.js",
+      "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
